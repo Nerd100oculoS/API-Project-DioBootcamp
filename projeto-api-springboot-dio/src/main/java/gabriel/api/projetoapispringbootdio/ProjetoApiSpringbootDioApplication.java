781a3dfa804515d7b89a34b045e52380d732ddfe
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2024-05-09 13:23:26 -0300

    projeto inicial upado

---
+++ projeto-api-springboot-dio/src/main/java/gabriel/api/projetoapispringbootdio/ProjetoApiSpringbootDioApplication.java
@@ -0,0 +1,13 @@
+package gabriel.api.projetoapispringbootdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiSpringbootDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiSpringbootDioApplication.class, args);
+	}
+
+}
---
+++ projeto-api-springboot-dio/src/test/java/gabriel/api/projetoapispringbootdio/ProjetoApiSpringbootDioApplicationTests.java
@@ -0,0 +1,13 @@
+package gabriel.api.projetoapispringbootdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiSpringbootDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
